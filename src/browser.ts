216981134ec52b72bbf488c399ce0952eb1da18e
fragment0 <hi@fragment0.com>
2018-11-12 19:50:35 +0800

    fix style insert bug

--- src/browser.ts
+++ src/browser.ts
@@ -1,6 +1,6 @@
 export const inject = (css: string) => {
   return `const style = document.createElement('style')
+          style.innerHTML = \`${css}\`
           document.head.appendChild(style)
-          style.sheet.insertRule(\`${css}\`)
           `
 }
